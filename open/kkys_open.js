72e4d155e9667caa5d2b81222df5928f037174bc
laoZ <71415980+xyzjhe@users.noreply.github.com>
2023-07-23 12:52:02 +0800

    Update kkys_open.js

--- open/kkys_open.js
+++ open/kkys_open.js
@@ -1,4 +1,4 @@
-import { Crypto, dayjs, jinja2, _ } from 'https://pan.shangui.cc/f/qmAZtG/cat.js';
+import { Crypto, dayjs, jinja2, _ } from './lib/cat.js';
 
 let key = 'kkys';
 let url = 'https://api1.baibaipei.com:8899';
